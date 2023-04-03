845f52fc451d41f5ed522066d9b66d3755dc7390
Demitomo <72909713+Demitomo@users.noreply.github.com>
2023-04-03 11:28:23 +0200

    Changement fichier

--- src/Pieces.py
+++ src/Pieces.py
@@ -1,39 +1,39 @@
 import pygame
 
-Brook = pygame.image.load("./Python/Chess/V3/Images/Brook.png")
+Brook = pygame.image.load("Images/Brook.png")
 Brook = pygame.transform.scale(Brook, (80, 80))
 
-Bknig = pygame.image.load("./Python/Chess/V3/Images/Bknig.png")
+Bknig = pygame.image.load("Images/Bknig.png")
 Bknig = pygame.transform.scale(Bknig, (80, 80))
 
-Bbish = pygame.image.load("./Python/Chess/V3/Images/Bbish.png")
+Bbish = pygame.image.load("Images/Bbish.png")
 Bbish = pygame.transform.scale(Bbish, (80, 80))
 
-Bqueen = pygame.image.load("./Python/Chess/V3/Images/Bqueen.png")
+Bqueen = pygame.image.load("Images/Bqueen.png")
 Bqueen = pygame.transform.scale(Bqueen, (80, 80))
 
-Bking = pygame.image.load("./Python/Chess/V3/Images/Bking.png")
+Bking = pygame.image.load("Images/Bking.png")
 Bking = pygame.transform.scale(Bking, (80, 80))
 
-Bpawn = pygame.image.load("./Python/Chess/V3/Images/Bpawn.png")
+Bpawn = pygame.image.load("Images/Bpawn.png")
 Bpawn = pygame.transform.scale(Bpawn, (80, 80))
 
-Wrook = pygame.image.load("./Python/Chess/V3/Images/Wrook.png")
+Wrook = pygame.image.load("Images/Wrook.png")
 Wrook = pygame.transform.scale(Wrook, (80, 80))
 
-Wknig = pygame.image.load("./Python/Chess/V3/Images/Wknig.png")
+Wknig = pygame.image.load("Images/Wknig.png")
 Wknig = pygame.transform.scale(Wknig, (80, 80))
 
-Wbish = pygame.image.load("./Python/Chess/V3/Images/Wbish.png")
+Wbish = pygame.image.load("Images/Wbish.png")
 Wbish = pygame.transform.scale(Wbish, (80, 80))
 
-Wqueen = pygame.image.load("./Python/Chess/V3/Images/Wqueen.png")
+Wqueen = pygame.image.load("Images/Wqueen.png")
 Wqueen = pygame.transform.scale(Wqueen, (80, 80))
 
-Wking = pygame.image.load("./Python/Chess/V3/Images/Wking.png")
+Wking = pygame.image.load("Images/Wking.png")
 Wking = pygame.transform.scale(Wking, (80, 80))
 
-Wpawn = pygame.image.load("./Python/Chess/V3/Images/Wpawn.png")
+Wpawn = pygame.image.load("Images/Wpawn.png")
 Wpawn = pygame.transform.scale(Wpawn, (80, 80))
 
 Pieces = ["r", "n", "b", "q", "k", "p", "R", "N", "B", "Q", "K", "P"]
