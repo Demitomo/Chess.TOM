cbfcddf1bc41cf622199194048bdf14a8659d391
pierrbt <pierre08bt@gmx.co.uk>
2023-05-07 00:57:41 +0200

    Reformattage du index.py

--- src/index.py
+++ src/index.py
@@ -1,4 +1,3 @@
-import pygame as py
 from pieces import *
 
 
@@ -13,7 +12,7 @@ class Board:
             ["-", "-", "-", "-", "-", "-", "-", "-"],
             ["-", "-", "-", "-", "-", "-", "-", "-"],
             ["P", "P", "P", "P", "P", "P", "P", "P"],
-            ["R", "N", "B", "Q", "K", "B", "N", "R"]
+            ["R", "N", "B", "Q", "K", "B", "N", "R"],
         ]
         self.selectedPiece = None
         self.checkMate = False
@@ -24,7 +23,8 @@ class Board:
     def PrintBoard(self):
         for y in range(8):
             for x in range(8):
-                if self.board[y][x] == "-": continue
+                if self.board[y][x] == "-":
+                    continue
 
                 Pieces[self.board[y][x]].DisplayPiece(x, y)
 
@@ -34,15 +34,19 @@ class Board:
                 self.running = False
 
             elif event.type == py.MOUSEBUTTONDOWN:
-                if event.button != 1: return
+                if event.button != 1:
+                    return
 
                 pos = py.mouse.get_pos()
                 x, y = int(pos[0] / 80), int(pos[1] / 80)
 
                 if self.selectedPiece is None:
-                    if self.board[y][x] == "-": return
+                    if self.board[y][x] == "-":
+                        return
                     if (self.board[y][x].islower() and self.whiteToPlay) or (
-                            self.board[y][x].isupper() and not self.whiteToPlay): return
+                        self.board[y][x].isupper() and not self.whiteToPlay
+                    ):
+                        return
 
                     self.selectedPiece = [x, y, self.board[y][x]]
 
@@ -52,7 +56,9 @@ class Board:
                         self.selectedPiece = None
                         return
 
-                    Move = Pieces[self.selectedPiece[2]].IsMoveLegal(self.board, self.selectedPiece[:2], dest)
+                    Move = Pieces[self.selectedPiece[2]].IsMoveLegal(
+                        self.board, self.selectedPiece[:2], dest
+                    )
 
                     if not Move:
                         return
@@ -79,7 +85,9 @@ class Board:
         self.screen.blit(bgImg, (0, 0))
 
         if self.selectedPiece is not None:
-            rect = py.rect.Rect(self.selectedPiece[0] * 80, self.selectedPiece[1] * 80, 80, 80)
+            rect = py.rect.Rect(
+                self.selectedPiece[0] * 80, self.selectedPiece[1] * 80, 80, 80
+            )
             py.draw.rect(self.screen, "yellow", rect)
 
         self.PrintBoard()
@@ -109,7 +117,7 @@ Pieces = {
     "B": Bishop("w"),
     "R": Rook("w"),
     "Q": Queen("w"),
-    "K": King("w")
+    "K": King("w"),
 }
 
 Game.Run()
