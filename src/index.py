f60c65aad55b00d2ed0716bfd8a942c61d2cdbca
pierrbt <pierre08bt@gmx.co.uk>
2023-05-07 00:41:59 +0200

    Corection égalités

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import pygame as py
 from pieces import *
 
+
 class Board:
     def __init__(self, screen) -> None:
         self.screen = screen
@@ -24,7 +25,7 @@ class Board:
         for y in range(8):
             for x in range(8):
                 if self.board[y][x] == "-": continue
-                
+
                 Pieces[self.board[y][x]].DisplayPiece(x, y)
 
     def Events(self):
@@ -34,16 +35,17 @@ class Board:
 
             elif event.type == py.MOUSEBUTTONDOWN:
                 if event.button != 1: return
-                
+
                 pos = py.mouse.get_pos()
                 x, y = int(pos[0] / 80), int(pos[1] / 80)
 
-                if self.selectedPiece == None:
+                if self.selectedPiece is None:
                     if self.board[y][x] == "-": return
-                    if (self.board[y][x].islower() and self.whiteToPlay) or (self.board[y][x].isupper() and not self.whiteToPlay): return
+                    if (self.board[y][x].islower() and self.whiteToPlay) or (
+                            self.board[y][x].isupper() and not self.whiteToPlay): return
 
                     self.selectedPiece = [x, y, self.board[y][x]]
-                
+
                 else:
                     dest = [x, y]
                     if dest == self.selectedPiece[:2]:
@@ -52,33 +54,34 @@ class Board:
 
                     Move = Pieces[self.selectedPiece[2]].IsMoveLegal(self.board, self.selectedPiece[:2], dest)
 
-                    if Move == False: return
+                    if not Move:
+                        return
                     else:
                         self.whiteToPlay = not self.whiteToPlay
                         self.board = Move
 
                         if self.selectedPiece[2] == "r":
                             if self.selectedPiece[:2] == [0, 0]:
-                                Pieces["k"].rookMoved[1] == True
+                                Pieces["k"].rookMoved[1] = True
                             elif self.selectedPiece[:2] == [7, 0]:
-                                Pieces["k"].rookMoved[0] == True
+                                Pieces["k"].rookMoved[0] = True
 
                         elif self.selectedPiece[2] == "R":
                             if self.selectedPiece[:2] == [0, 7]:
-                                Pieces["K"].rookMoved[1] == True
+                                Pieces["K"].rookMoved[1] = True
                             elif self.selectedPiece[:2] == [7, 7]:
-                                Pieces["K"].rookMoved[0] == True
-                        
+                                Pieces["K"].rookMoved[0] = True
+
                         self.selectedPiece = None
 
     def Display(self):
         self.screen.fill("black")
-        self.screen.blit(bgImg, (0,0))
+        self.screen.blit(bgImg, (0, 0))
 
-        if self.selectedPiece != None:
+        if self.selectedPiece is not None:
             rect = py.rect.Rect(self.selectedPiece[0] * 80, self.selectedPiece[1] * 80, 80, 80)
             py.draw.rect(self.screen, "yellow", rect)
-        
+
         self.PrintBoard()
 
         py.display.flip()
@@ -88,7 +91,7 @@ class Board:
             self.Events()
             self.Display()
             self.fps.tick(60)
-            
+
 
 py.display.set_caption("Chess.tom")
 
